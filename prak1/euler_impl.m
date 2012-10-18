aa5ba5bb560fb18378e5eafd89a24ad259d53e03
Hexren <oliver@steenbuck.org>
2012-10-18 14:19:01 +0200

    euler impl

--- prak1/euler_impl.m
+++ prak1/euler_impl.m
@@ -6,7 +6,9 @@ function [x, y] = euler_impl(begin, h, xend, func)
     vec_y_tmp=[yn];
 
     for xn1 = xn:h:xend
-        yn1 = yn + h * func(xn,yn);
+        options = optimset('Display', 'off');
+        yn1Approx = fsolve(@(n)n-yn-h*func(xn1,n), xn1, options);
+        yn1 = yn + h * func(xn1, yn1Approx);
         
         vec_y_tmp=[vec_y_tmp,yn1];
         vec_x_tmp=[vec_x_tmp,xn1];
--- prak1/stiff.m
+++ prak1/stiff.m
@@ -2,6 +2,7 @@ h = 0.001;
 vec_ana_x = 0:h:0.2;
 vec_ana_y = mtp0101_ana(vec_ana_x);
 [vec_eulerexpl_x, vec_eulerexpl_y] = euler_expl(1, h, 0.2, @f);
+[vec_eulerimpl_x, vec_eulerimpl_y] = euler_impl(1, h, 0.2, @f);
 [vec_runge_x, vec_runge_y] = rungeKutta(1, h, 0.2, @f);
 
 
@@ -9,6 +10,7 @@ subplot(1,2,1)
 hold on;
 plot(vec_ana_x, vec_ana_y, 'r');
 plot(vec_eulerexpl_x, vec_eulerexpl_y, 'k');
+plot(vec_eulerimpl_x, vec_eulerimpl_y, 'g');
 plot(vec_runge_x, vec_runge_y, 'b');
 grid on;
 title('Approximation');
