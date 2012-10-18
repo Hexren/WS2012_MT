3d4fe409a77806867b78d2551a2fabe4b4f36740
Hexren <oliver@steenbuck.org>
2012-10-18 15:34:33 +0200

    add: error

--- prak1/stiff.m
+++ prak1/stiff.m
@@ -1,4 +1,4 @@
-h = 0.001;
+h = 0.004;
 vec_ana_x = 0:h:0.2;
 vec_ana_y = mtp0101_ana(vec_ana_x);
 [vec_eulerexpl_x, vec_eulerexpl_y] = euler_expl(1, h, 0.2, @f);
@@ -15,16 +15,15 @@ plot(vec_runge_x, vec_runge_y, 'b');
 grid on;
 title('Approximation');
 legend('analytische Loesung', 'Vorwaerts Euler', 'Rueckwaerts Euler', 'Runge-Kutta');
-axis([0, 0.05, 0, 0.2]);
+axis([0, 0.2, -1, 3]);
 
 subplot(1,2,2)
 
 hold on;
-plot(vec_ana_x, vec_ana_y - vec_ana_y, 'r');
-plot(vec_eulerexpl_x, vec_ana_y - vec_eulerexpl_y, 'k');
-plot(vec_eulerimpl_x, vec_ana_y - vec_eulerimpl_y, 'g');
-plot(vec_runge_x, vec_ana_y - vec_runge_y, 'b');
+plot(vec_eulerexpl_x, vec_eulerexpl_y - vec_ana_y, 'k');
+plot(vec_eulerimpl_x, vec_eulerimpl_y - vec_ana_y, 'g');
+plot(vec_runge_x, vec_runge_y - vec_ana_y, 'b');
 grid on;
 title('Error');
-legend('analytische Loesung', 'Vorwaerts Euler','Rueckwaerts Euler', 'Runge-Kutta');
-axis([0,0.05, -0.15, 0.15]);
+legend( 'Vorwaerts Euler','Rueckwaerts Euler', 'Runge-Kutta');
+axis([0,0.2, -1.5, 1.5]);
