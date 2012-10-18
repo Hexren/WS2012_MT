0796a64ad5f2f8f5f86e452e337fd666d57cb054
Hexren <oliver@steenbuck.org>
2012-10-18 11:14:59 +0200

    add: euler impl

---
+++ prak1/euler_impl.m
@@ -0,0 +1,20 @@
+function [x, y] = euler_impl(begin, h, xend, func)
+    yn=begin;
+    xn=0;
+    
+    vec_x_tmp=[xn];
+    vec_y_tmp=[yn];
+
+    for xn1 = xn:h:xend
+        yn1 = yn + h * func(xn,yn);
+        
+        vec_y_tmp=[vec_y_tmp,yn1];
+        vec_x_tmp=[vec_x_tmp,xn1];
+        
+        xn=xn1;
+        yn=yn1;
+    end
+    y = vec_y_tmp;
+    x = vec_x_tmp;
+end
+
