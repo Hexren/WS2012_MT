fda2bdad60a828a03aeb118fba85ba0ac5b68147
Oliver Steenbuck <git@hexren.net>
2012-10-20 23:08:05 +0200

    add: xkcdfy

--- prak1/stiff.m
+++ prak1/stiff.m
@@ -7,8 +7,13 @@ vec_ana_y = mtp0101_ana(vec_ana_x);
 
 
 fh = figure('color','w');
-subplot(1,2,1)
+screen_size = get(0, 'ScreenSize');
+set(fh, 'Position', [0 0 screen_size(3) screen_size(4) ] );
+a = subplot(1,2,1);
 set(gca,'fontName','Comic Sans MS','fontSize',18,'lineWidth',3,'box','off')
+
+lw = 3;
+
 
 %annotation(fh,'textarrow',[0.38 0.34],[0.42 0.55],...
 %     'string',sprintf('analytische Loesung',char(10)),'headStyle','none','lineWidth',1.5,...
@@ -16,36 +21,16 @@ set(gca,'fontName','Comic Sans MS','fontSize',18,'lineWidth',3,'box','off')
 
 
 hold on;
-plot(vec_ana_x, vec_ana_y, 'r','lineWidth',3);
-plot(vec_eulerexpl_x, vec_eulerexpl_y, 'k','lineWidth',3);
-plot(vec_eulerimpl_x, vec_eulerimpl_y, 'g','lineWidth',3);
-plot(vec_runge_x, vec_runge_y, 'b','lineWidth',3);
-grid on;
+plot(vec_ana_x, vec_ana_y, 'r','lineWidth',lw);
+plot(vec_eulerexpl_x, vec_eulerexpl_y, 'k','lineWidth',lw);
+plot(vec_eulerimpl_x, vec_eulerimpl_y, 'g','lineWidth',lw);
+plot(vec_runge_x, vec_runge_y, 'b','lineWidth',lw);
+grid off;
 title('Approximation');
 legend('analytische Loesung', 'Vorwaerts Euler', 'Rueckwaerts Euler', 'Runge-Kutta');
-axis([0, 0.04, 0, 0.5]);
+axis([0, 0.02, 0, 0.4]);
 
-%# capture with export_fig
-im = export_fig('-nocrop', fh);
-
-%# add a bit of border to avoid black edges
-im = padarray(im,[15 15 0],255);
-
-%# make distortion grid
-sfc = size(im);
-[yy,xx]=ndgrid(1:7:sfc(1),1:7:sfc(2));
-pts = [xx(:),yy(:)];
-tf = cp2tform(pts+randn(size(pts)),pts,'lwm',12);
-w = warning;
-warning off images:inv_lwm:cannotEvaluateTransfAtSomeOutputLocations
-imt = imtransform(im,tf);
-warning(w)
-
-%# remove padding
-imt = imt(16:end-15,16:end-15,:);
-
-figure('color','w')
-imshow(imt)
+%xkcdify(a)
 
 
 
